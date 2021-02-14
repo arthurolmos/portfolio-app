cf68f80912250e3d64c44d675ac5d1716ae547a8
Arthur Olmos Wosniaki <arthur.wosniaki@totvs.com.br>
2021-02-14 15:06:25 -0300

    Fixing portfolio page layout

--- src/data/items/portfolioItems.ts
+++ src/data/items/portfolioItems.ts
@@ -1,12 +1,12 @@
 import { IPortfolioItem } from "../../interfaces/IPortfolioItem";
 
 export const portfolioItems: IPortfolioItem[] = [
-  {
-    backgroundImage: "/images/portfolio/portfolio-app/home.jpg",
-    title: "Portfolio",
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet, dui eget rhoncus elementum, velit enim ultricies nisl, vehicula volutpat magna tortor vitae odio. Suspendisse nulla magna, fringilla vitae massa nec, accumsan porttitor dui. In hac habitasse platea dictumst. Maecenas aliquam mauris id metus viverra fermentum. Phasellus consequat a turpis sed rhoncus. Vestibulum vel pellentesque turpis, mollis pulvinar dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Nullam pretium eleifend urna at varius. Ut eu sapien fringilla, ornare ante eget, lacinia ante. Donec vel luctus augue. Ut molestie purus vel felis venenatis, nec egestas nisl sagittis. In accumsan elementum interdum. Donec efficitur tincidunt nibh, quis scelerisque magna. Etiam nibh dolor, ornare ac mauris eget, facilisis faucibus dolor. Praesent eget nibh et neque vulputate auctor ut sed orci. Aenean velit velit, euismod sit amet egestas sed, molestie quis est. Maecenas sollicitudin elementum risus ac accumsan. Sed egestas felis eu diam feugiat, sagittis consequat tellus auctor. Nullam nec vulputate nulla, vitae suscipit sapien. Nam quis lorem tempus, suscipit quam ut, porta nisl.`,
-  },
+  // {
+  //   backgroundImage: "/images/portfolio/portfolio-app/home.jpg",
+  //   title: "Portfolio",
+  //   text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet, dui eget rhoncus elementum, velit enim ultricies nisl, vehicula volutpat magna tortor vitae odio. Suspendisse nulla magna, fringilla vitae massa nec, accumsan porttitor dui. In hac habitasse platea dictumst. Maecenas aliquam mauris id metus viverra fermentum. Phasellus consequat a turpis sed rhoncus. Vestibulum vel pellentesque turpis, mollis pulvinar dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  //   Nullam pretium eleifend urna at varius. Ut eu sapien fringilla, ornare ante eget, lacinia ante. Donec vel luctus augue. Ut molestie purus vel felis venenatis, nec egestas nisl sagittis. In accumsan elementum interdum. Donec efficitur tincidunt nibh, quis scelerisque magna. Etiam nibh dolor, ornare ac mauris eget, facilisis faucibus dolor. Praesent eget nibh et neque vulputate auctor ut sed orci. Aenean velit velit, euismod sit amet egestas sed, molestie quis est. Maecenas sollicitudin elementum risus ac accumsan. Sed egestas felis eu diam feugiat, sagittis consequat tellus auctor. Nullam nec vulputate nulla, vitae suscipit sapien. Nam quis lorem tempus, suscipit quam ut, porta nisl.`,
+  // },
   {
     backgroundImage: "/images/portfolio/rolezator-app/rolezator.jpg",
     title: "Rolezator",
