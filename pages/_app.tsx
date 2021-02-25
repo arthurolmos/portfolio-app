e22aee52fd9a11784fe08ca13e0709b227dbc10d
Arthur Olmos Wosniaki <arthur.wosniaki@totvs.com.br>
2021-02-25 00:02:14 -0300

    Advancing...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { SideMenu, TopMenu } from "../src/components/menus";
-import type { AppProps /*, AppContext */ } from "next/app";
-import "../styles/global.css";
+import type { AppProps } from "next/app";
+import "../src/assets/styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
