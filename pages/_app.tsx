71bb7254cd9f153167dd36d6708424d589375cad
Arthur Olmos Wosniaki <arthur.wosniaki@totvs.com.br>
2021-03-12 09:52:20 -0300

    Fixing background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
+import React from "react";
 import { SideMenu, TopMenu } from "../src/components/menus";
 import type { AppProps } from "next/app";
 import "../src/assets/styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  let height;
+
+  React.useEffect(() => {
+    height = window.innerHeight;
+  }, []);
+
   return (
-    <>
+    <div style={{ height: height }}>
       <SideMenu />
       <TopMenu />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
